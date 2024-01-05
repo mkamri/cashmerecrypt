f1994bb9680d6b278134ea8354ee03c96fc667ab
mkamri <mail@mikamarie.com>
2024-01-04 22:55:33 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include($_SERVER["DOCUMENT_ROOT"].'/header.html'); ?>
+<main>
+    <section id="welcome">
+        <h1>welcome</h1>
+        <p>Hi there! My name is Mika and this is my little corner of the internet.</p>
+        <p>My goal is to create a space to host my projects and share things that interest me. Please make yourself at home!</p>
+    </section>
+    <section id="about">
+        <h1>about me</h1>
+        <p>I'm an artist and web developer with an interest in gothic themes and minimalism. I love to discuss philosophy, taxonomy, counterculture, art, and the nature of life itself.</p>
+    </section>
+    <section id="latest">
+        <h1>latest</h1>
+        <p>
+            <time datetime="2024-01-04">1/4/2023</time>: 
+            Create the basic layout for Cashmere Crypt
+        </p>
+    </section>
+</main>
+<?php include($_SERVER["DOCUMENT_ROOT"].'/nav.html'); ?>
+<?php include($_SERVER["DOCUMENT_ROOT"].'/footer.html'); ?>
